67853d680599f150410190a4154f92adf8030856
Guy Bruneau <guy.bruneau@gmail.com>
2021-12-13 08:34:36 -0500

    base

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
+
 $(function () {
     $("#layers").accordion({
-        heightStyle: "content"
+        heightStyle: "auto",
+        collapsible: true
     });
 });
 
@@ -16,34 +18,63 @@ $(function () {
 
 });
 
+/* === Dialog box functions for references ===*/
+
 $(function () {
     // Dialog Caracteristics
-    $("#diagRef").dialog(
+    $(".gbDialog").dialog(
         {
             autoOpen: false,
-            minWidth: 700,
+            minWidth: 500,
             position: {
                 my: "top",
                 at: "center top"
-            } ,
-            hide: 'explode',
-            show: 'puff'
+            },
+            hide: {effect: 'fade',  duration: 500  },
+            show: {effect: 'fade',  duration: 500  },
+            duration: 50
         }
     );
     // On click event         
 
-    $(".arcExample").click(function () {
+    $(".gbHelp").click(function () {
         $("#diagRef").dialog("open");
     });
-
 });
 
-function loadHtml(aSelection,url) {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function () {
-        $(aSelection).append(this.responseText);
-    }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
+/* === For the Examples Dialog === */
+
+$(function () {
+    var w = window.innerWidth - 500;
+    var h = window.innerHeight -50;
+    $("#diagExample").dialog({
+        autoOpen: false,
+        modal: false,
+        hide: {effect: 'fade',duration: 500  },
+        show: {effect: 'fade',  duration: 500  },
+        height: "auto",
+        width: w,
+        position: { my: "right top", at: "right top"}
+    });
+});
+
+$("#diagExample").on("dialogopen",function(event, ui) {
+    $(".ui-dialog-titlebar-close").hide();
+});   
+
+
+$(document).ready(function(){
+    $(".arcExample").on("mouseenter",function () {
+        $("#diagExample img").attr("src", $(this).attr("src"));
+        $("#diagExample").click(function () {
+            $("#diagExample").dialog("close");
+        });
+/*        $("#diagExample").parent().mouseleave(function () {
+            $("#diagExample").dialog("close");
+        }); */
+        $("#diagExample").dialog("open"); 
+    });
+});
+
+
 
