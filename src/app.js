ede12c7b766f3021cb67b7c86b709e577549727e
Guy Bruneau <guy.bruneau@gmail.com>
2021-12-13 16:37:34 -0500

    Final V1

--- src/app.js
+++ src/app.js
@@ -1,80 +1,58 @@
-
 $(function () {
-    $("#layers").accordion({
-        heightStyle: "auto",
-        collapsible: true
-    });
-});
-
-$(function () {
-    $(".gbTabs").tabs();
 
     $(function () {
-        $(document).tooltip({
-            track: true,
-            position: { my: "right-50 bottom", at: "left bottom", collision: "flipfit" }
+        $("#layers").accordion({
+            heightStyle: "content",
+            collapsible: true
         });
     });
 
-});
+    $(function () {
+        $(".gbTabs").tabs();
 
-/* === Dialog box functions for references ===*/
-
-$(function () {
-    // Dialog Caracteristics
-    $(".gbDialog").dialog(
-        {
-            autoOpen: false,
-            minWidth: 500,
-            position: {
-                my: "top",
-                at: "center top"
-            },
-            hide: {effect: 'fade',  duration: 500  },
-            show: {effect: 'fade',  duration: 500  },
-            duration: 50
-        }
-    );
-    // On click event         
-
-    $(".gbHelp").click(function () {
-        $("#diagRef").dialog("open");
-    });
-});
-
-/* === For the Examples Dialog === */
-
-$(function () {
-    var w = window.innerWidth - 500;
-    var h = window.innerHeight -50;
-    $("#diagExample").dialog({
-        autoOpen: false,
-        modal: false,
-        hide: {effect: 'fade',duration: 500  },
-        show: {effect: 'fade',  duration: 500  },
-        height: "auto",
-        width: w,
-        position: { my: "right top", at: "right top"}
-    });
-});
-
-$("#diagExample").on("dialogopen",function(event, ui) {
-    $(".ui-dialog-titlebar-close").hide();
-});   
-
-
-$(document).ready(function(){
-    $(".arcExample").on("mouseenter",function () {
-        $("#diagExample img").attr("src", $(this).attr("src"));
-        $("#diagExample").click(function () {
-            $("#diagExample").dialog("close");
+        $(function () {
+            $(document).tooltip({
+                track: true,
+                position: { my: "right-50 bottom", at: "left bottom", collision: "flipfit" }
+            });
         });
-/*        $("#diagExample").parent().mouseleave(function () {
-            $("#diagExample").dialog("close");
-        }); */
-        $("#diagExample").dialog("open"); 
+
     });
+
+    /* === Dialog box functions for references ===*/
+
+    $(function () {
+        // Dialog Caracteristics
+        $(".gbDialog").dialog(
+            {
+                autoOpen: false,
+                minWidth: 500,
+                position: {
+                    my: "top",
+                    at: "center top"
+                },
+                hide: { effect: 'fade', duration: 500 },
+                show: { effect: 'fade', duration: 500 },
+                duration: 50
+            }
+        );
+        // On click event         
+
+        $(".gbHelp").click(function () {
+            $("#diagRef").dialog("open");
+        });
+    });
+
+    /*   Add Zoom  */
+
+    var newImg;
+    $(".arcExample").each(function () {
+        src=$(this).attr('src');
+        newImg = '<div class="container"><div class="zoom"><img  src=' + src + '></div></div>';
+        $(newImg).insertAfter(this);
+        console.log(newImg);
+        $(this).css('display', 'none');
+    })
+    
+    zoom();
 });
-
-
-
